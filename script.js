4443672c27339c089544c1c422aa0e27f77c0ad0
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2025-12-21 15:24:01 +0530

    Added while loop to the code

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 let humanscore = 0;
 let computerscore = 0;
+let keepGoing = true;
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+let humanChoice = "";
+let computerChoice = "";
 
-playRound(humanChoice, computerChoice);
+while (keepGoing) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    if (humanChoice == "quit") {
+        let winner = (humanscore == computerscore) ? "No One" : (humanscore > computerscore) ? "Player" : "Computer";
+        console.log(`Your Score: ${humanscore} \nComputer Score: ${computerscore} \n${winner} WINS!!!`);
+        keepGoing = false;
+    } else {
+        playRound(humanChoice, computerChoice);
+    }
+}
+
 
 function playRound(humanChoice, computerChoice) {
     if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissor") || (humanChoice == "scissor" && computerChoice == "rock")) {
         computerscore++;
+        console.log(`Your Choice: ${humanChoice} \n Computer Choice: ${computerChoice}`);
         console.log("Computer WINS!");
         console.log(`Your Score: ${humanscore} \nComputer Score: ${computerscore}`)
-    } else {
+    } else if (humanChoice === computerChoice) {
+        console.log(`Your Choice: ${humanChoice} \n Computer Choice: ${computerChoice}`);
+        console.log("It's a TIE!!!");
+        console.log(`Your Score: ${humanscore} \nComputer Score: ${computerscore}`)
+    }
+    else {
         humanscore++;
+        console.log(`Your Choice: ${humanChoice} \n Computer Choice: ${computerChoice}`);
         console.log("You WIN!");
         console.log(`Your Score: ${humanscore} \nComputer Score: ${computerscore}`)
     }
@@ -20,14 +39,14 @@ function playRound(humanChoice, computerChoice) {
 
 function getComputerChoice() {
     num = Math.floor(Math.random() * 3);
-    choice = num == 0 ? "rock" : num == 1 ? "paper" : num == 2 ? "scissors" : "not possible";
+    choice = num == 0 ? "rock" : num == 1 ? "paper" : num == 2 ? "scissor" : "not possible";
     return choice;
 }
 
 function getHumanChoice() {
-    let choice = prompt("Enter your choice (rock, paper, or scissors):");
+    let choice = prompt("Enter your choice (rock, paper, scissor, or quit):");
     choice = choice.toLowerCase();
-    if (choice == "rock" || choice == "paper" || choice == "scissors") {
+    if (choice == "rock" || choice == "paper" || choice == "scissor" || choice == "quit") {
         return choice;
     } else {
         alert("Please enter a valid choice!");
